486499bf95b3985b6a44242ec7f915dd25c97449
Lydiagraveline <lydiagraveline20@gmail.com>
2023-10-20 14:14:45 -0400

    got connected

--- exercise1/index.js
+++ exercise1/index.js
@@ -1,28 +1,105 @@
+let express = require('express');
+const portNumber = 8080;
+const app = express(); //make an instance of express
+const server = require("http").createServer(app);
+
+
+// create a server (using the Express framework object)
+app.use(express.static(__dirname + "/public"));
+
+//whats dis
+app.use(express.json()); // support json encoded bodies
+app.use(express.urlencoded({ extended: true })); // support encoded bodies
+
+app.use("/client", clientRoute);
+
+require("dotenv").config();
+
+//app.use("/client", clientRoute);
+app.listen(portNumber, function () {
+  console.log("Server is running on port "+portNumber);
+});
+
 //tutorial https://www.mongodb.com/developer/languages/javascript/node-crud-tutorial/
 const { MongoClient } = require('mongodb');
+const url = process.env.MONGO_DB_URI;
 
-async function main(){
-    const uri = "mongodb+srv://lydiagraveline20:huffpuff123@cluster1.4ht5yiw.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(url, {});
 
-    const client = new MongoClient(uri);
-    try {
-        await client.connect();
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    client.connect().then((res) => {
+      console.log("connected");
 
-        await listDatabases(client);
-    } catch {
-        console.error(e);
-    } finally{
-        await client.close();
-    }
+    })
+
+
+  } 
+  catch(error) {
+    console.error("error::");
+    console.log(error);
+
+  }
+  finally {
+    await client.close();
+  }
 }
+
+run();
+
+function clientRoute(req, res, next) {
+  res.sendFile(__dirname + "/public/client.html");
+}
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(url, {});
+// async function run(){
+//     try {
+//         client.connect();
+//         console.log("connected");
+
+//         // await listDatabases(client);
+//        // let db = await client.db("reddit_posts"); //reference the database
+//         //let helpful = await db.collection("helpfulRedditPosts"); // the collection
+
+
+//         //let query = await collection.findOne({"postTitle": {$regex: "groceries", "$options": "i"}});
+//        // let query = await helpful.aggregate([{ $sample: { size: 1 } } ]);
+//        // console.log(query);
+
+//     } catch {
+//         console.error(e);
+//     } finally{
+//         await client.close();
+//     }
+// }
 
 // call the function
-main().catch(console.error);
+///run().catch(console.error);
 
-async function listDatabases(client){
-    const databasesList = await client.db().admin().listDatabases();
-    console.log("Databases:");
-    databasesList.databases.forEach(db => {
-        console.log(`- ${db.name}`);
-    })
-}
+// async function listDatabases(client){
+//     const databasesList = await client.db().admin().listDatabases();
+//     //console.log("Databases:");
+//     databasesList.databases.forEach(db => {
+//        // console.log(`- ${db.name}`);
+//     })
+// }
+
+// make server listen for incoming messages
+// server.listen(portNumber, function () {
+//     console.log("listening on port:: " + portNumber);
+//   });
+
+//default route
+// app.get("/", function (req, res) {
+//     res.send("<h1>Hello world</h1>");
+//   });
+
+
+  // function clientRoute(req, res, next) {
+  //   res.sendFile(__dirname + "/public/client.html");
+  // }
---
+++ exercise1/public/js/client.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    console.log("we are loaded");
+
+//GET
+document.querySelector("#findData").addEventListener('click', 
+  
+async function(){
+  // let lat_lng_vals = {lat_send:document.querySelector("#search_lat").value, lng_send:document.querySelector("#search_long").value}
+  // let  response = await fetch(`http://localhost:4200/sendSearch?${new URLSearchParams(lat_lng_vals)}`);
+  // console.log("the response::: ")
+  // console.log(await response.json());
+
+});//click
+
+}
