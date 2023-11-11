d66b37370f28f3e93277772b2b8a97fc04a41ab6
Lydiagraveline <lydiagraveline20@gmail.com>
2023-11-11 17:51:09 -0500

    display the header and ephemeris in the sketch

--- JPLHorizons-Demo/public/sketch.js
+++ JPLHorizons-Demo/public/sketch.js
@@ -1,28 +1,75 @@
-let apiUrl = "http://localhost:3000/api"; //fetching from server
-let targetBody = "Mercury"; // Change to the desired planet
-let startTime = "2023-01-01";
-let endTime = "2023-12-31";
-let stepSize = "1d";
+let headerInfo;
+let ephemerisData;
 
-let query = `format=text&COMMAND='${targetBody}'&EPHEM_TYPE=OBSERVER&OBJ_DATA=NO&START_TIME='${startTime}'&STOP_TIME='${endTime}'&STEP_SIZE='${stepSize}'`;
+function processHeader(data) {
+  const lines = data.trim().split('\n');
+  let headerInfo = {};
+  for (const line of lines) {
+    const [key, value] = line.split(':').map(item => item.trim());
+    if (key && value) {
+      headerInfo[key] = value;
+    }
+  }
+  return headerInfo;
+}
 
-let planetsData;
+function processEphemeris(data) {
+  const lines = data.trim().split('\n').slice(24);
+  const ephemerisData = lines.map(line => {
+    const [date, time, ra, dec, apmag, sbrt, delta, deldot, sot_r, sto_r, cnst] = line.trim().split(/\s+/);
+    return {
+      date,
+      time,
+      ra,
+      dec,
+      apmag,
+      sbrt,
+      delta,
+      deldot,
+      sot_r,
+      sto_r,
+      cnst,
+    };
+  });
+  return ephemerisData;
+}
+
+function renderSolarSystem() {
+  // Display header information
+  textSize(16);
+  text('Header Information:', 20, 30);
+  let yPos = 50;
+  for (const key in headerInfo) {
+    text(`${key}: ${headerInfo[key]}`, 20, yPos);
+    yPos += 20;
+  }
+
+  // Display ephemeris data
+  textSize(14);
+  text('\nEphemeris Data:', 20, yPos + 20);
+  yPos += 40;
+
+  for (const entry of ephemerisData) {
+    const displayText = `${entry.date} ${entry.time} - RA: ${entry.ra}, Dec: ${entry.dec}`;
+    text(displayText, 20, yPos);
+    yPos += 20;
+  }
+}
 
 function setup() {
-    createCanvas(400, 400);
-    fetchData();
-  }
+  createCanvas(windowWidth, windowHeight);
   
-  function draw() {
-    background(220);
-  }
+  // Use the local server API to fetch data
+  const apiUrl = '/api?format=text&COMMAND=\'499\'&OBJ_DATA=\'NO\'&MAKE_EPHEM=\'YES\'&EPHEM_TYPE=\'OBSERVER\'&CENTER=\'500@399\'&START_TIME=\'2006-01-01\'&STOP_TIME=\'2006-01-20\'&STEP_SIZE=\'1%20d\'&QUANTITIES=\'1,3,4,9,20,23,24,29\'';
 
-// Fetch data from JPL Horizons
-function fetchData() {
-    //fetch(`https://ssd.jpl.nasa.gov/api/horizons.api?format=text&COMMAND='499'&OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='OBSERVER'&CENTER='500@399'&START_TIME='2006-01-01'&STOP_TIME='2006-01-20'&STEP_SIZE='1%20d'&QUANTITIES='1,9,20,23,24,29'`)
-   fetch(`${apiUrl}?${query}`)
-    .then((response) => response.text())
-    .then((data) => processData(data));
-  }
+  fetch(apiUrl)
+    .then(response => response.text())
+    .then(data => {
+      headerInfo = processHeader(data);
+      ephemerisData = processEphemeris(data);
+      renderSolarSystem();
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
 
 
--- JPLHorizons-Demo/server.js
+++ JPLHorizons-Demo/server.js
@@ -9,10 +9,20 @@ app.use(express.static('public'));
  app.get('/api', async (req, res) => {
   const apiUrl = 'https://ssd.jpl.nasa.gov/api/horizons.api';
   const queryParams = req.query;
-  const apiResponse = await fetch(`${apiUrl}?${new URLSearchParams(queryParams)}`);
-  const data = await apiResponse.text();
-  res.send(data);
+  try {
+    const apiResponse = await fetch(`${apiUrl}?${new URLSearchParams(queryParams)}`);
+    const data = await apiResponse.text();
+    res.send(data);
+  } catch {
+    console.error('Error fetching data:', error);
+    res.status(500).send('Internal Server Error');
+  }
+
  });
+
+ app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
