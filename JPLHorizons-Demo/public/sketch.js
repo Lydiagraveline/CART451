000e850d9a56d406fc0c358ac131eedd77e7dba8
Lydiagraveline <lydiagraveline20@gmail.com>
2023-11-11 17:16:39 -0500

    handle API on server side

--- JPLHorizons-Demo/public/sketch.js
+++ JPLHorizons-Demo/public/sketch.js
@@ -1,4 +1,4 @@
-let apiUrl = "https://ssd.jpl.nasa.gov/api/horizons.api";
+let apiUrl = "http://localhost:3000/api"; //fetching from server
 let targetBody = "Mercury"; // Change to the desired planet
 let startTime = "2023-01-01";
 let endTime = "2023-12-31";
@@ -19,8 +19,8 @@ function setup() {
 
 // Fetch data from JPL Horizons
 function fetchData() {
-    fetch(`https://ssd.jpl.nasa.gov/api/horizons.api?format=text&COMMAND='499'&OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='OBSERVER'&CENTER='500@399'&START_TIME='2006-01-01'&STOP_TIME='2006-01-20'&STEP_SIZE='1%20d'&QUANTITIES='1,9,20,23,24,29'`)
-   //fetch(`${apiUrl}?${query}`)
+    //fetch(`https://ssd.jpl.nasa.gov/api/horizons.api?format=text&COMMAND='499'&OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='OBSERVER'&CENTER='500@399'&START_TIME='2006-01-01'&STOP_TIME='2006-01-20'&STEP_SIZE='1%20d'&QUANTITIES='1,9,20,23,24,29'`)
+   fetch(`${apiUrl}?${query}`)
     .then((response) => response.text())
     .then((data) => processData(data));
   }
--- JPLHorizons-Demo/server.js
+++ JPLHorizons-Demo/server.js
@@ -1,1 +1,19 @@
 const express = require('express'); 
+//const fetch = require('node-fetch');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+ app.get('/api', async (req, res) => {
+  const apiUrl = 'https://ssd.jpl.nasa.gov/api/horizons.api';
+  const queryParams = req.query;
+  const apiResponse = await fetch(`${apiUrl}?${new URLSearchParams(queryParams)}`);
+  const data = await apiResponse.text();
+  res.send(data);
+ });
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
