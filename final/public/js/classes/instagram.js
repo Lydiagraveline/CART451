67136a3078364c2a3f115f45e1ab0638158526f9
Lydiagraveline <lydiagraveline20@gmail.com>
2023-11-29 19:40:37 -0500

    class folder

---
+++ final/public/js/classes/instagram.js
@@ -0,0 +1,17 @@
+class Instagram{
+    constructor(data){
+        this.chat = data.title;
+    } 
+}
+
+//Saving for later
+
+//  function parseIGData() {
+//     let chats = [];
+//     let participants = [];
+//     for (let i=0; i<inbox.length; i++){
+//       chats.push(inbox[i].title);
+//       participants.push(inbox[i].participants);
+//     }
+//     console.log(participants);
+//   }
--- final/public/js/main.js
+++ final/public/js/main.js
@@ -12,9 +12,8 @@ async function fetchData(path, className) {
           throw new Error('Network response was not ok');
         }
         const result = await response.json();
-        console.log(path, result);
-        // console.log(result.length);
         const classObject = createClassObj(result, className);
+        // console.log(classObject);
         return classObject; //return the data as an object
       } catch (error) {
         console.error('Error fetching hinge data:', error);
@@ -26,18 +25,19 @@ async function fetchData(path, className) {
 async function preload(){
     // Fetch data and assign them to the variables
    hingeData = await fetchData('/hingeData');//initialize();
-   instagramData = await fetchData('/instagramData');
+   instagramData = await fetchData('/instagramData', Instagram);
    images = await fetchData('/mediaData', Image);
 }
 
 function createClassObj(result, className){
-    if(result, className){ //wait for the data to be fetched, and for the class to be defined
+    if(result && className){ //wait for the data to be fetched, and for the class to be defined
         let array = [];
         for(let i=0; i<result.length; i++){ 
          array.push(new className(result[i]));
         }
          return array;
     } else {
+        console.log("class not defined");
         return [];
     }
 }
@@ -52,19 +52,3 @@ function draw(){
     background(245);
 
  }
-
-
-
-
-
-//Saving for later
-
-//  function parseIGData() {
-//     let chats = [];
-//     let participants = [];
-//     for (let i=0; i<inbox.length; i++){
-//       chats.push(inbox[i].title);
-//       participants.push(inbox[i].participants);
-//     }
-//     console.log(participants);
-//   }
